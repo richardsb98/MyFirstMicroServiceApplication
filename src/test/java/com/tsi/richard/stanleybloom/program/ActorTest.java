1ae02b7885f10ea0060ee0ed13fd1f9dc1dff7c3
Richard <richard281298@hotmail.co.uk>
2022-06-07 17:25:15 +0100

    Testing coverage

--- src/test/java/com/tsi/richard/stanleybloom/program/ActorTest.java
+++ src/test/java/com/tsi/richard/stanleybloom/program/ActorTest.java
@@ -7,10 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ActorTest extends Actor {
 
-    Actor testActor = new Actor ("first name", "last name");
+    Actor testActor = new Actor ("first_name", "last_name");
     @Test
     public void testActor(){
-        assertEquals("error", "first_name", "");
+        Assertions.assertEquals("first_name", testActor.getFirst_name(), "first_name is not initialised");
+        Assertions.assertEquals("last_name", testActor.getLast_name(), "last_name is not initialised");
+
     }
 
 
